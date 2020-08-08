0c62517ccad2d2c5018906255ff64a8126923294
Alex Diaz <dicardenasd@unal.edu.co>
2020-08-08 01:11:37 -0500

    implemented User CRUD functions: - list all registers - delete

--- WebApp/Controllers/HomeController.cs
+++ WebApp/Controllers/HomeController.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using WebApp.Models;
+using WebApp.Models.DB;
 
 namespace WebApp.Controllers
 {
@@ -20,10 +21,60 @@ namespace WebApp.Controllers
 
         public IActionResult Index()
         {
-            return View();
+            List<Usuario> allUsers = new List<Usuario>();
+            using (var DB = new webapptestdbContext())
+            {
+                allUsers = DB.Usuario.ToList();
+            }
+
+            return View(allUsers);
+        }
+        
+        [HttpGet]
+        public IActionResult newUser()
+        {
+            User user = new User();
+            return View(user);
+        }
+        
+        [HttpPost]
+        public IActionResult newUser(User user)
+        {
+            if (ModelState.IsValid)
+            {
+                using (var db = new webapptestdbContext())
+                {
+                    Usuario userDB = new Usuario()
+                    {
+                        Name = user.Name
+                    };
+                    db.Usuario.Add(userDB);
+                    db.SaveChanges();
+                }
+
+                return RedirectToAction("Index");
+            }
+            return View(user);
+        }
+        
+        [HttpGet]
+        public IActionResult delete(int idUser)
+        {
+            using (var DB = new webapptestdbContext())
+            {
+                Usuario userDB = new Usuario();
+                userDB = DB.Usuario.Find(idUser);
+                if (userDB != null)
+                {
+                    DB.Remove(userDB);
+                    DB.SaveChanges();
+                }
+            }
+
+            return Redirect("/");
         }
 
-        public IActionResult Privacy()
+        public IActionResult Other()
         {
             return View();
         }
---
+++ WebApp/Models/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
+
+namespace WebApp.Models
+{
+    public class User
+    {
+        public int IdUsuario { get; set; }
+        
+        [Required(ErrorMessage = "Campo obligatorio")]
+        [StringLength(45, ErrorMessage = "Máximo 45 caracteres")]
+        public string Name { get; set; }
+    }
+}
